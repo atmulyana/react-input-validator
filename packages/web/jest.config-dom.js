e761572630de99f2411c31fd1746610e36ef6bf5
AT Mulyana <atmulyana@yahoo.com>
2025-07-02 07:06:05 +0700

    fixes "pakage.json" of all packages
    `transformIgnorePatterns`  of jest config seems not needed

--- packages/web/jest.config-dom.js
+++ packages/web/jest.config-dom.js
@@ -2,8 +2,5 @@ module.exports = {
     testEnvironment: "jsdom",
     testMatch: [
         "**/__tests__/Context.test.js"
-    ],
-    transformIgnorePatterns: [
-        "node_modules/(?!(javascript-common|@react-native|react-native|reactjs-common)/)"
-    ],
+    ]
 };
