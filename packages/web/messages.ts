f7df15463c8bc87cc3fb8ad7c3f0248e8a884871
AT Mulyana <atmulyana@yahoo.com>
2025-07-10 13:59:08 +0700

    updates package.json

--- packages/web/messages.ts
+++ packages/web/messages.ts
@@ -1,5 +1,5 @@
 /**
  * https://github.com/atmulyana/react-input-validator
  */
-import messages from '@react-input-validator/rules/messages';
+import messages from '@react-input-validator/core/messages';
 export default messages;
